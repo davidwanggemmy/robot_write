2c7802bd551ae2e4941d75dc0132e796aca8c11f
davidwanggemmy <davidwanggemmy@outlook.com>
2019-12-10 20:04:56 +0800

    4

--- ws/build/robot_write/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wang/Downloads/ws/src/robot_write
-# Build directory: /home/wang/Downloads/ws/build/robot_write
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- ws/build/sample_vision/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wang/Downloads/ws/src/sample_vision
-# Build directory: /home/wang/Downloads/ws/build/sample_vision
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
